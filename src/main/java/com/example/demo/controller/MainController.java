2b256cfad5bf812ebeee8d47fcc7042a4ccccfe3
LiamPKU <supermiaozheng@163.com>
2020-02-11 15:33:21 +0800

    fk

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -3,7 +3,6 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 //主页控制器
 @Controller//返回同名页面
